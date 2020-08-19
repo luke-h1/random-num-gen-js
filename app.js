b4fea884d66c2d259c656bf32f413d7ad5220b94
luke-h1 <luke.howsam@yahoo.com>
2020-08-19 13:46:07 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn2 = document.getElementById('random').addEventListener('click', genRanNum);
+
+
+
+function genRanNum(){
+  let i = Math.floor((Math.random() * 20) + 1); 
+  const a = document.querySelector('.res1'); 
+  a.innerHTML = i;   
+  console.log(i);
+}
+
